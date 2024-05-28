e8167ac1d73ea2fd6e053b2833925dff8cce191d
DDoudine <amandinekemp@gmail.com>
2024-05-28 15:25:24 +0200

    Refactor HBNB web application code for consistency and readability

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,6 +1,8 @@
-#!/usr/bin/python3
-""" Starts a Flash web application for HBNB """
 
+#!/usr/bin/python3
+""" Starts a Flash Web Application for HBNB """
+
+# Import required modules and classes
 from models import storage
 from models.state import State
 from models.city import City
@@ -8,19 +10,22 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
-from uuid import uuid4
+import uuid
 
+# Create a Flask web application instance
 app = Flask(__name__)
 
-# The following lines remove unnecessary whitespace in the HTML output
+# Uncomment the following lines to remove unnecessary whitespace in the HTML output
 # app.jinja_env.trim_blocks = True
 # app.jinja_env.lstrip_blocks = True
 
+# Close the database connection after each request
 @app.teardown_appcontext
 def close_db(error):
-    """ Remove the current SQLAlchemy session """
+    """ Remove the current SQLAlchemy Session """
     storage.close()
 
+# Define a route for the HBNB web application
 @app.route('/0-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
@@ -41,17 +46,14 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    # Generate a new UUID for caching
-    cache_id = uuid4()
-
-    # Render the HTML template and pass the fetched data as variables
-    return render_template('100-hbnb.html',
+    # Generate a new UUID for caching and pass it to the template
+    return render_template('0-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
-                           cache_id=cache_id
-                           )
+                           cache_id=uuid.uuid4())
 
+# Main function
 if __name__ == "__main__":
-    """ Main function """
+    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
