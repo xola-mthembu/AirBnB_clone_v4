005c0bd82daa500d8a5cc859e4b0ebdef4f4c43b
Livanhernandez <livanhernandez9@gmail.com>
2024-05-28 19:36:12 -0400

    Add - Added file with amenities working.

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -1,17 +1,47 @@
+#!/usr/bin/python3
+""" Starts a Flash Web Application """
+from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from os import environ
 from flask import Flask, render_template
-import uuid
-import os
-from models import storage  # Assuming you have a storage module to handle data retrieval
-
+from uuid import uuid4
 app = Flask(__name__)
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
 
-@app.route('/1-hbnb/')
+
+@app.teardown_appcontext
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+
+@app.route('/1-hbnb/', strict_slashes=False)
 def hbnb():
-    cache_id = uuid.uuid4()
-    states = storage.all("State").values()  # Retrieve all states
-    amenities = storage.all("Amenity").values()  # Retrieve all amenities
-    places = storage.all("Place").values()  # Retrieve all places
-    return render_template('1-hbnb.html', cache_id=cache_id, states=states, amenities=amenities, places=places)
+    """ HBNB is alive! """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    st_ct = []
+
+    for state in states:
+        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
+
+    amenities = storage.all(Amenity).values()
+    amenities = sorted(amenities, key=lambda k: k.name)
+
+    places = storage.all(Place).values()
+    places = sorted(places, key=lambda k: k.name)
+
+    return render_template('1-hbnb.html',
+                           states=st_ct,
+                           amenities=amenities,
+                           places=places,
+                           cache_id = uuid4())
+
 
 if __name__ == "__main__":
+    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
