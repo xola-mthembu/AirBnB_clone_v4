37312dc7f4ecaacc2c01218e7b67704f7d9823c1
DDoudine <amandinekemp@gmail.com>
2024-05-29 14:10:11 +0200

    Add ajax

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -69,19 +69,17 @@ $('document').ready(function () {
               </ARTICLE>`;
     }));
 
-  // Initialize an empty object to store selected amenities
-  const listAmenities = {};
-  
-  // Listen for changes on checkbox inputs
-  $('input[type="checkbox"]').change(function () {
-    // If the checkbox is checked, add the amenity to the list
-    if ($(this).is(':checked')) {
-      listAmenities[$(this).attr('data-id')] = $(this).attr('data-name');
-    } else {
-      // If the checkbox is unchecked, remove the amenity from the list
-      delete listAmenities[$(this).attr('data-id')];
-    }
-    // Update the text of the amenities H4 element to show selected amenities
-    $('.amenities H4').text(Object.values(listAmenities).join(', '));
-  });
+    $('button').click(function () {
+      $.ajax({
+        url: urlPlaces,
+        type: 'POST',
+        data: JSON.stringify({ 'amenities': Object.keys(listAmenities) }),
+        contentType: 'application/json',
+        dataType: 'json',
+        success: newPlaces
+      },
+      )
+    });
+
+  }
 });
