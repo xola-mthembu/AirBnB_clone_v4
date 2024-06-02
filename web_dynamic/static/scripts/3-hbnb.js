37fb32625cbdf9f43b3d4e91234a6e9c6ccf68a1
xola-mthembu <xola.mthembu@gmail.com>
2024-06-02 11:14:11 -0700

    4. Fetch places - Dynamically load places from the API

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,17 +1,6 @@
 $(document).ready(function () {
-  const selectedAmenities = {};
-
-  $('input[type="checkbox"]').change(function () {
-    if (this.checked) {
-      selectedAmenities[$(this).data('id')] = $(this).data('name');
-    } else {
-      delete selectedAmenities[$(this).data('id')];
-    }
-    const amenities = Object.values(selectedAmenities).join(', ');
-    $('.amenities h4').text(amenities);
-  });
-
-  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+  const apiUrl = 'http://0.0.0.0:5001/api/v1/status/';
+  $.get(apiUrl, function (data) {
     if (data.status === 'OK') {
       $('#api_status').addClass('available');
     } else {
@@ -19,28 +8,39 @@ $(document).ready(function () {
     }
   });
 
+  const amenityDict = {};
+  $('input[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenityDict[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenityDict[$(this).data('id')];
+    }
+    $('.amenities h4').text(Object.values(amenityDict).join(', '));
+  });
+
+  const placesSearchUrl = 'http://0.0.0.0:5001/api/v1/places_search/';
   $.ajax({
-    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    url: placesSearchUrl,
     type: 'POST',
-    contentType: 'application/json',
     data: JSON.stringify({}),
+    contentType: 'application/json',
     success: function (data) {
       for (const place of data) {
         $('.places_container').append(
           `<article>
-             <div class="title_box">
-               <h2>${place.name}</h2>
-               <div class="price_by_night">$${place.price_by_night}</div>
-             </div>
-             <div class="information">
-               <div class="max_guest">${place.max_guest} Guests</div>
-               <div class="number_rooms">${place.number_rooms} Bedrooms</div>
-               <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
-             </div>
-             <div class="description">
-               ${place.description}
-             </div>
-           </article>`
+            <div class="title_box">
+              <h2>${place.name}</h2>
+              <div class="price_by_night">$${place.price_by_night}</div>
+            </div>
+            <div class="information">
+              <div class="max_guest">${place.max_guest} Guest(s)</div>
+              <div class="number_rooms">${place.number_rooms} Bedroom(s)</div>
+              <div class="number_bathrooms">${place.number_bathrooms} Bathroom(s)</div>
+            </div>
+            <div class="description">
+              ${place.description}
+            </div>
+          </article>`
         );
       }
     }
