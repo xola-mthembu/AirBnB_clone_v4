6a17fe3a3a2e9a8acceb3bc318f1d85bc93099d2
xola-mthembu <xola.mthembu@gmail.com>
2024-06-02 11:17:45 -0700

    5. Filter places by Amenity - Implement filtering places based on selected amenities

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+  const apiUrl = 'http://0.0.0.0:5001/api/v1/status/';
+  $.get(apiUrl, function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+
+  const amenityDict = {};
+  $('input[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenityDict[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenityDict[$(this).data('id')];
+    }
+    $('.amenities h4').text(Object.values(amenityDict).join(', '));
+  });
+
+  function fetchPlaces (amenities = {}) {
+    const placesSearchUrl = 'http://0.0.0.0:5001/api/v1/places_search/';
+    $.ajax({
+      url: placesSearchUrl,
+      type: 'POST',
+      data: JSON.stringify({ amenities: Object.keys(amenities) }),
+      contentType: 'application/json',
+      success: function (data) {
+        $('.places_container').empty();
+        for (const place of data) {
+          $('.places_container').append(
+            `<article>
+              <div class="title_box">
+                <h2>${place.name}</h2>
+                <div class="price_by_night">$${place.price_by_night}</div>
+              </div>
+              <div class="information">
+                <div class="max_guest">${place.max_guest} Guest(s)</div>
+                <div class="number_rooms">${place.number_rooms} Bedroom(s)</div>
+                <div class="number_bathrooms">${place.number_bathrooms} Bathroom(s)</div>
+              </div>
+              <div class="description">
+                ${place.description}
+              </div>
+            </article>`
+          );
+        }
+      }
+    });
+  }
+
+  fetchPlaces();
+
+  $('button').click(function () {
+    fetchPlaces(amenityDict);
+  });
+});
